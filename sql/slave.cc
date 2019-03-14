017222e77d4a63a6eafe74271adb6342445d61c5
Sujatha Sivakumar <sujatha.sivakumar@mariadb.com>
2019-03-14 12:05:18 +0530

    MDEV-13895: GTID and Master_Delay causes excessive initial delay
    Problem:
    ========
    When attempting to delay a Slave attached with GTID, there appears to be an
    extra delay applied initially. For example, this output reflects a Slave that is
    already delayed by 43200 seconds. When switching to GTID replication,
    replication is paused until SQL_Remaining_Delay counts down to 0:
    
    CHANGE MASTER TO master_use_gtid=current_pos; CHANGE MASTER TO
    MASTER_DELAY=43200;
    
    Seconds_Behind_Master: 44847
    Using_Gtid: Current_Pos
    SQL_Delay: 43200
    SQL_Remaining_Delay: 43089
    Slave_SQL_Running_State: Waiting until MASTER_DELAY seconds after master
    executed event
    
    Analysis:
    =========
    When slave initiates a GTID based connection request to master, the master sends
    two GTID_LIST events.  The first one is actual GTID_LIST event and the second
    one is a fake GTID_LIST event. This is sent by master to provide its current
    binlary log file position. The fake GTID_LIST events will have their ev->when=0.
    'when' (the timestamp) is set to 0 so that slave could distinguish between real
    and fake Rotate events.
    
    On slave side when MASTER_DELAY is configured to "X" the applier will ensure
    that there is a time delay of "X" seconds before the event is applied.
    
    General behaviour of MASTER_DELAY example:-
    
    Master
    timestamp of event e1=10
    timestamp of event e2=11
    
    On slave MASTER_DELAY=5
    Event e1 will be applied at = 15
    e2 will be applied at =16
    
    In bug scenario:-
    
    On Master: With GTIDs
    timestamp of event e1=10
    timestamp of event e2=0
    
    On Slave:
    e1 will be applied at = 10 + 5 =15
    For e2, since "e2->when=0" e2->when is set to current timestamp.
    i.e since the e2->when and current timestamp on slave is the same applier waits
    for additional master_delay=5 seconds. the ev->when contributes to
    "rli->last_master_timestamp".
    
    rli->last_master_timestamp= ev->when + (time_t) ev->exec_time;
    
    Fake events should not update the "ev->when" to "current timestamp" on slave
    instead they should point to the latest "rli->last_master_timestamp".
    
    Fix:
    ===
    For fake events set the "ev->when" to most recent "rli->last_master_timestamp".

--- sql/slave.cc
+++ sql/slave.cc
@@ -3532,12 +3532,6 @@ apply_event_and_update_pos_setup(Log_event* ev, THD* thd, rpl_group_info *rgi)
   thd->variables.server_id = ev->server_id;
   thd->set_time();                            // time the query
   thd->lex->current_select= 0;
-  if (!ev->when)
-  {
-    my_hrtime_t hrtime= my_hrtime();
-    ev->when= hrtime_to_my_time(hrtime);
-    ev->when_sec_part= hrtime_sec_part(hrtime);
-  }
   thd->variables.option_bits=
     (thd->variables.option_bits & ~OPTION_SKIP_REPLICATION) |
     (ev->flags & LOG_EVENT_SKIP_REPLICATION_F ? OPTION_SKIP_REPLICATION : 0);
@@ -3889,11 +3883,18 @@ static int exec_relay_log_event(THD* thd, Relay_log_info* rli,
       the user might be surprised to see a claim that the slave is up to date
       long before those queued events are actually executed.
      */
-    if (!rli->mi->using_parallel() &&
-        !(ev->is_artificial_event() || ev->is_relay_log_event() || (ev->when == 0)))
+    if (!rli->mi->using_parallel())
     {
-      rli->last_master_timestamp= ev->when + (time_t) ev->exec_time;
-      DBUG_ASSERT(rli->last_master_timestamp >= 0);
+      if (!(ev->is_artificial_event() || ev->is_relay_log_event() || (ev->when == 0)))
+      {
+        rli->last_master_timestamp= ev->when + (time_t) ev->exec_time;
+	DBUG_ASSERT(rli->last_master_timestamp >= 0);
+      }
+      /*
+        For fake rotate event update its timestamp to the latest laster_master_timestamp.
+      */
+      if (ev->when == 0)
+        ev->when= (my_time_t)rli->last_master_timestamp;
     }
 
     /*
@@ -3970,9 +3971,13 @@ static int exec_relay_log_event(THD* thd, Relay_log_info* rli,
         */
         if (ev->get_type_code() != FORMAT_DESCRIPTION_EVENT)
           rli->last_master_timestamp= ev->when + (time_t) ev->exec_time;
-
         DBUG_ASSERT(rli->last_master_timestamp >= 0);
       }
+      /*
+        For fake rotate event update its timestamp to the latest laster_master_timestamp.
+      */
+      if (ev->when == 0)
+        ev->when= (my_time_t)rli->last_master_timestamp;
     }
 
     if (typ == GTID_EVENT)
